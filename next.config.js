899255841164d8f3c2570e40f2e9e6753b0c3514
intangible-media <steven@intangiblemedia.com>
2024-08-13 14:02:01 -0700

    tasting again

--- next.config.js
+++ next.config.js
@@ -15,6 +15,11 @@ const nextConfig = {
         pathname: "/**", // Allows all paths on this domain
       },
       {
+        protocol: "https", // Use 'https' for production
+        hostname: "worthy-cats-5f3ad8d62a.media.strapiapp.com", // Specify the production API hostname
+        pathname: "/**", // Allows all paths on this domain
+      },
+      {
         protocol: "http", // Use 'http' for localhost
         hostname: "localhost", // Specify 'localhost' as the hostname
         port: "1337", // Specify the port, if necessary
