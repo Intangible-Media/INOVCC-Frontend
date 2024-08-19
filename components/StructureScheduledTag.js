9e8305337c57dedebab7e4efad0718774fd35d07
intangible-media <steven@intangiblemedia.com>
2024-08-19 13:40:52 -0700

    updated the tag

--- components/StructureScheduledTag.js
+++ components/StructureScheduledTag.js
@@ -3,12 +3,17 @@ import { formatDateToMonthDay } from "../utils/strings";
 export default function StructureScheduledTag({ structure }) {
   const { scheduleStart, scheduleEnd } = structure.attributes;
 
-  const formatedStartDate = formatDateToMonthDay(
-    structure.attributes.scheduleStart
-  );
-  const formatedEndDate = formatDateToMonthDay(
-    structure.attributes.scheduleEnd
-  );
+  const options = { month: "short", day: "numeric" };
+
+  const formatLocalDate = (dateString) => {
+    const [year, month, day] = dateString.split("-");
+    const date = new Date(year, month - 1, day); // Months are 0-based in JS
+    return date.toLocaleDateString("en-US", options);
+  };
+
+  const formattedStartDate = formatLocalDate(scheduleStart);
+  const formattedEndDate = formatLocalDate(scheduleEnd);
+
   return (
     <div className="flex gap-2">
       <h5 className="font-medium text-xs leading-none text-gray-800">
@@ -16,7 +21,7 @@ export default function StructureScheduledTag({ structure }) {
       </h5>
       {scheduleStart && scheduleEnd ? (
         <p className="font-medium text-xs leading-none text-gray-500">
-          {`${formatedStartDate} - ${formatedEndDate}`}
+          {`${formattedStartDate} - ${formattedEndDate}`}
         </p>
       ) : (
         <p className="font-medium text-xs leading-none text-gray-500">
